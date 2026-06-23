69ce3f6b6335a333020f4002c1f06e578cf3730b
Josué Lustosa <josuelustosa.job@gmail.com>
2026-06-23 15:33:09 -0400

    chore(deps): add redux persist stack and test preset

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   preset: '@react-native/jest-preset',
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|react-redux|@reduxjs|redux|redux-persist|immer|reselect|@react-native-async-storage)/)',
+  ],
 };
