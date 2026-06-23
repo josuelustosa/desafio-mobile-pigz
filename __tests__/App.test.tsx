6d2e5736d197528dd7203fb54d78bd65fcef30e5
Josué Lustosa <josuelustosa.job@gmail.com>
2026-06-23 15:37:37 -0400

    test(app): add smoke test for store wiring

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,3 +1,34 @@
 /**
  * @format
  */
+
+import React from 'react';
+import renderer, { act } from 'react-test-renderer';
+
+import App from '../src/App';
+import { persistor } from '../src/store';
+
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest'),
+);
+
+describe('App', () => {
+  afterEach(async () => {
+    await persistor.flush();
+    persistor.pause();
+  });
+
+  it('renders the initial screen', async () => {
+    let component: renderer.ReactTestRenderer;
+
+    await act(async () => {
+      component = renderer.create(<App />);
+    });
+
+    expect(component!.toJSON()).toBeTruthy();
+
+    await act(async () => {
+      component!.unmount();
+    });
+  });
+});
